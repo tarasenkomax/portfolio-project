dbf4c70aafa11d88b6ad41e1fac4fecb2695add1
Tarasenko Maxim <Wick71@yandex.ru>
2020-01-07 15:04:02 +0300

    Create model in jobs and add migration

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,3 +108,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'

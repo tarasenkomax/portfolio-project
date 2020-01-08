d3cfe1b411038fa23de73b3a545fe4a3dc9544d5
Tarasenko Maxim <Wick71@yandex.ru>
2020-01-08 23:52:06 +0300

    Jobs app up and running

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -3,7 +3,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -15,11 +14,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'jobs.apps.JobsConfig',
+    'jobs.apps.JobsConfig',  # Add!!!!!!!!!!
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,17 +57,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',  # Change (замена стандартной бд на psql)
+        'NAME': 'portfolio',  # Change   (имя бд)
+        'USER': 'postgres',  # Add       (имя пользователя)
+        'PASSWORD': '7567',  # Add       (пароль)
+        'HOST': 'localhost',  # Add      (адрес бд)
+        'PORT': '5432',  # Add           (порт сервера бд)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -89,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -103,10 +102,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Add
+MEDIA_URL = '/media/'  # Add
--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings  # Add
+from django.conf.urls.static import static  # Add
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

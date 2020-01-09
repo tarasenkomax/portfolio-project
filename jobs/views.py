147f44235c199f76c2a29a83822dcf4d8c0f9ef1
Tarasenko Maxim <Wick71@yandex.ru>
2020-01-09 15:41:14 +0300

    Added bootstrap and jobs are showing up

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Job  # Для доступа к базе данных
 
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
